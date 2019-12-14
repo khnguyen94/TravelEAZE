2b9db2706f6de1b8bd6ce6b25cb8656e2336a8d0
cprybell <cprybell@sbcglobal.net>
2019-12-14 12:56:21 -0800

    Updated the post route for reservations. Need to pull before I test though

--- public/js/reservations.js
+++ public/js/reservations.js
@@ -1,7 +1,34 @@
-// Get references to page elements
-var $departureCity = $("#departure-airport-input");
-var $arrivalCity = $("#destination-airport-input");
-var $departureDate = $("#departure-date-input");
-var $returnDate = $("#return-date-input");
-var $loginSubmit = $("#reservation-submit");
+$(document).ready(function() {
+  // Get references to page elements
+  var $departureCity = $("#departure-airport-input");
+  var $arrivalCity = $("#destination-airport-input");
+  var $departureDate = $("#departure-date-input");
+  var $returnDate = $("#return-date-input");
+  var resForm = $("#new-reservation");
 
+  $(resForm).on("submit", function handleFormSubmit(event) {
+    event.preventDefault();
+    // Wont submit the post if we are missing a body or a title
+    if (
+      !$departureCity.val().trim() ||
+      !$arrivalCity.val().trim() ||
+      !$departureDate.val().trim() ||
+      !$returnDate.val().trim()
+    ) {
+      return;
+    }
+    var newPost = {
+      startDate: $departureDate.val().trim(),
+      endDate: $returnDate.val().trim(),
+      departureLoc: $departureCity.val(),
+      arrivalLoc: $arrivalCity.val()
+    };
+    console.log(newPost);
+    submitPost(newPost);
+  });
+  function submitPost(Post) {
+    $.post("/api/resorts/", Post, function() {
+      window.location.href = "/reservations";
+    });
+  }
+});
