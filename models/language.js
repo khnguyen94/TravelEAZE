c3bac69376ca036c89a2957ed0294b7ffa0b0bbe
Kyle LaMont <kylerlamont@gmail.com>
2019-12-16 19:27:38 -0800

    added timestamp:false to language.js to avoid creating timestamp columns in table

--- models/language.js
+++ models/language.js
@@ -1,12 +1,18 @@
 // Create a "Language" model that matches up with the database
 module.exports = function(sequelize, DataTypes) {
-  var Language = sequelize.define("Language", {
-    languageAbb: DataTypes.STRING,
-    languageName: DataTypes.STRING,
-    LanguageCont: DataTypes.STRING,
-    LanguageCap: DataTypes.STRING,
-    LanguageCurr: DataTypes.STRING,
-    LanguageCode: DataTypes.STRING
-  });
+  var Language = sequelize.define(
+    "Language",
+    {
+      languageAbb: DataTypes.STRING,
+      languageName: DataTypes.STRING,
+      LanguageCont: DataTypes.STRING,
+      LanguageCap: DataTypes.STRING,
+      LanguageCurr: DataTypes.STRING,
+      LanguageCode: DataTypes.STRING
+    },
+    {
+      timestamps: false
+    }
+  );
   return Language;
 };
